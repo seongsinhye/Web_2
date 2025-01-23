56ab6fc6962129b35fb0ec9c6ef7ed46139919db
seongsinhye <sinhye2428@naver.com>
2025-01-23 22:40:36 +0900

    Initial Commit

---
+++ Web_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Web_2
+//
+//  Created by 성신혜 on 1/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Web_2Tests/Web_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Web_2Tests.swift
+//  Web_2Tests
+//
+//  Created by 성신혜 on 1/23/25.
+//
+
+import Testing
+@testable import Web_2
+
+struct Web_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
